857ccda9645d199495f718ad5cc7f8d65dea53fb
Idan Peleg <idan@younow.com>
2018-12-19 13:40:59 -0500

    export SettlePayload

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,12 @@
 import { TransactionManager, SubmitAPIResponse, BatchDetailsTransaction } from './transaction_manager';
 import TransactionManagerOptions from './transaction_manager_options';
 import IssuePayload from './payloads/issue_payload';
+import SettlePayload from './payloads/settle_payload';
 
 export {
   TransactionManager,
   IssuePayload,
+  SettlePayload,
   SubmitAPIResponse,
   BatchDetailsTransaction,
   TransactionManagerOptions
